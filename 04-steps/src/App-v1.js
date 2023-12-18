4847aab05e404b825cc0116bcf54f28ba2eb8bed
junaidtk <junaid9037@gmail.com>
2023-12-18 20:56:19 +0530

    Added children concepts in the code

--- 04-steps/src/App-v1.js
+++ 04-steps/src/App-v1.js
@@ -44,11 +44,7 @@ export default function App() {
         </p>
 
         <div className="buttons">
-          <Button textColor="#fff" bgColor="#7950f2" onClick={handlePrevious}><span></span>Previous</Button>
-
-          <Button textColor="#fff" bgColor="#7950f2" onClick={handleNext}>Next<span></span></Button>
-          
-          {/* <button
+          <button
             style={{ backgroundColor: "#7950f2", color: "#fff" }}
             onClick={handlePrevious}
           >
@@ -59,18 +55,10 @@ export default function App() {
             onClick={handleNext}
           >
             Next
-          </button> */}
+          </button>
         </div>
       </div>
       ) }
     </div>
   )
-}
-
-function Button({textColor, bgColor, onClick, children}){
-  return (
-    <button style={{backgroundColor:bgColor, color:textColor }} onClick={onClick}>
-      {children}
-    </button>
-  );
 }

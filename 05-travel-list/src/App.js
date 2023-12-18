1bc1bcc5a9bab6276c62721c10d43358b20fc0df
junaidtk <junaid9037@gmail.com>
2023-12-18 17:26:34 +0530

    sort section added

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -71,10 +71,28 @@ function Form({onAddItem}){
 }
 
 function PackingList({items, onDeleteItem, onToggleItem}){
+    const [sorytBy, setSorytBy] = useState('input');
+
+    let sortedItems;
+
+    if(sorytBy === 'input') sortedItems = items;
+
+    if(sorytBy === 'description') sortedItems = items.slice().sort((a, b)=>a.description.localeCompare(b.description));
+
+    if(sorytBy === 'packed') sortedItems = items.slice().sort((a, b)=> Number(a.packed) - Number(b.packed));
+
   return <div className="list">
     <ul>
-      {items.map(item=> <Item item={item} onDeleteItem={()=>onDeleteItem(item.id)} onToggleItem={()=>onToggleItem(item.id)} />)}
+      {sortedItems.map(item=> <Item item={item} onDeleteItem={()=>onDeleteItem(item.id)} onToggleItem={()=>onToggleItem(item.id)} />)}
     </ul>
+
+    <div className="actions">
+        <select value={sorytBy} onChange={(e) => setSorytBy(e.target.value)}>
+            <option value="input">Sort by input order</option>
+            <option value="description">Sort by description order</option>
+            <option value="packed">Sort by status</option>
+        </select>
+    </div>
   </div>
 }
 
